705852bd4f5a041502ae802a5e52a8a5da516065
Ypurek <oleksii.ostapov@gmail.com>
2018-01-12 00:27:54 +0200

    updated core models structure

--- core/models.py
+++ core/models.py
@@ -15,20 +15,29 @@ class Project(models.Model):
 class Tag(models.Model):
     name = models.CharField(default='',
                             null=False,
-                            max_length=100)
+                            max_length=100,
+                            unique=True)
+    description = models.CharField(default=None,
+                                   null=True,
+                                   max_length=140)
 
 
 class Status(models.Model):
     status = models.CharField(default='',
                               null=False,
-                              max_length=50)
+                              max_length=50,
+                              unique=True)
 
 
 class TestCase(models.Model):
-    name = models.CharField(default='',
-                              null=False,
-                              max_length=100)
+    summary = models.CharField(default='',
+                               null=False,
+                               max_length=100)
     tags = models.ManyToManyField(Tag)
+    body = models.CharField(default=None,
+                            null=True,
+                            max_length=2000)
+    version = models.SmallIntegerField(default=1)
 
 
 class TestRun(models.Model):
@@ -39,14 +48,38 @@ class TestRun(models.Model):
                                default=None,
                                null=True,
                                on_delete=models.SET_NULL)
+    timestamp = models.DateTimeField(default=dt.datetime.now(),
+                                     null=False)
     tags = models.ManyToManyField(Tag)
     comment = models.CharField(default='',
                                max_length=1000)
+    version = models.SmallIntegerField(default=1)
 
 
+# TODO plan to remove. Make test run copy on TR change
 class TestRunHistory(models.Model):
     test_run = models.ForeignKey(TestRun,
                                  on_delete=models.CASCADE,
                                  related_name='history')
     timestamp = models.DateTimeField(default=dt.datetime.now(),
                                      null=False)
+    status = models.ForeignKey(Status,
+                               default=None,
+                               null=True,
+                               on_delete=models.SET_NULL)
+    comment = models.CharField(default=None,
+                               null=True,
+                               max_length=1000)
+
+
+class TestCaseHistory(models.Model):
+    test_case = models.ForeignKey(TestCase,
+                                  on_delete=models.CASCADE,
+                                  related_name='history')
+    summary = models.CharField(default=None,
+                               null=True,
+                               max_length=100)
+    body = models.CharField(default=None,
+                            null=True,
+                            max_length=2000)
+    version = models.SmallIntegerField(default=1)
