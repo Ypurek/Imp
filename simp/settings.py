63c3d9dde1bcc332674c7e869a96b0b34788b26e
Ypurek <oleksii.ostapov@gmail.com>
2018-01-15 23:54:02 +0200

    added social auth requirement

--- simp/settings.py
+++ simp/settings.py
@@ -31,12 +31,18 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'core.apps.CoreConfig',
+    'ui.apps.UiConfig',
+    'user_mgr.apps.UserMgrConfig',
+
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'social_django',
 ]
 
 MIDDLEWARE = [
